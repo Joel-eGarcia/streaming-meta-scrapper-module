6fa94643c176a53c6a4d2861c5a1a87a4d78d737
rala-l <81008522+rala-l@users.noreply.github.com>
2023-09-01 17:41:59 +0300

    Update environments.js

--- environments.js
+++ environments.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const envs = {
 	nfxUser: process.env.SMSM_NFX_USER || null,
-	nfxPass: process.env.SMSM_NFX_PASSWORRD || null,
+	nfxPass: process.env.SMSM_NFX_PASSWORD || null,
 	nfxAccountOriginCoutryCode: process.env.SMSM_NFX_ORIGIN_COUNTRY_CODE || 'RO',
 	privatePath: process.env.PRIVATE_DIR || path.resolve(__dirname, './private'),
 };
