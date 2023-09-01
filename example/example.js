2237e8668bfb9599b27a9ae9befe47b45bf313c0
rala-l <81008522+rala-l@users.noreply.github.com>
2023-09-01 16:13:46 +0300

    Update example.js

--- example/example.js
+++ example/example.js
@@ -1,20 +1,13 @@
 require('dotenv').config()
-const { smsm } = require('./../index');
-//const fs = require('fs');
+const smsm = require('./../index');
 
 (async () => {
 	const scrapper = new smsm({
-		//name: 'The Blacklist',
 		name: 'Better Things',
-		//id: 'tt2741602',
 		id: 'tt4370596',
 		providers: ['nfx', 'hbm', 'dnp'],
 		locale: 'ro-RO',
 	});
 	const metadata = await scrapper.getMetadata();
 	console.log(metadata);
-	//if (metadata) {
-		//await fs.promises.writeFile('./' + metadata.ids.imdb + '.json', JSON.stringify(metadata, null, 2));
-	//}
-	
 })();
